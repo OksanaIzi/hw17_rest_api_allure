e2835f54af55e1786b19c929eb3b1a8c1319d677
Oksana Izibekova <oksana.izibekova@mail.com>
2021-05-23 16:19:50 +0300

    добавлены тесты Rest API

---
+++ src/test/java/models/Books.java
@@ -0,0 +1,30 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "books"
+})
+
+public class Books {
+
+    @JsonProperty("books")
+    private List<Book> books = null;
+
+    @JsonProperty("books")
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @JsonProperty("books")
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+}
+
